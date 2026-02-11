16aac82bcd33e816b1e2144de3cf86391ba9bee6
mikeMigUelSg <up202107620@up.pt>
2026-02-11 20:44:08 +0000

    review 1

--- client/src/components/Technology/EnergySystem3D.jsx
+++ client/src/components/Technology/EnergySystem3D.jsx
@@ -703,7 +703,7 @@ function VPPHouse({ position, index }) {
       </mesh>
 
       {/* Painel solar no telhado frontal */}
-      <group position={[0, 3.3, 0.85]} rotation={[roofAngle, 0, 0]}>
+      <group position={[0, 3.5, 0.85]} rotation={[roofAngle, 0, 0]}>
         <mesh castShadow>
           <boxGeometry args={[2.4, 0.06, 1.6]} />
           <meshPhysicalMaterial
